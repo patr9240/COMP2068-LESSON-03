e7739be3cc1f8b5fe4add41f7947b8e4a8c2fbbe
Patrick Ross <patr9240@gmail.com>
2016-01-30 16:36:59 -0500

    updated server.js to run express

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 /// <reference path="_reference.ts" />
-var http = require('http');
+//creating express app
+var express = require('express');
+var app = express();
 var port = process.env.port || 3000;
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//main route
+app.get('/', function (req, res, next) {
+    res.send('Hello Express!');
+});
+//Route for info page
+app.get('/info', function (req, res) {
+    res.send('Your custom info page goes here');
+});
+app.listen(port, function () {
+    console.log("app Server Started... on port: " + port);
+});
+/*
+//creating node app
+import http = require('http');
+
+var port:number = process.env.port || 3000
+
+var server:http.Server = http.createServer(
+    function(req:http.ServerRequest, res:http.ServerResponse){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end("Hello Node!");
 });
-server.listen(port, function () {
-    console.log("Server Started... Listening on port: " + port);
+server.listen(port, function(){
+   console.log("Server Started... Listening on port: " + port);
 });
+*/
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,4 +1,32 @@
 /// <reference path="_reference.ts" />
+//creating express app
+import express = require('express');
+var app:express.Express = express();
+var port:number = process.env.port || 3000;
+//main route
+app.get('/', function (req:express.Request, res:express.Response, next:any){
+    res.send('Hello Express!');
+});
+
+//Route for info page
+app.get('/info', function(req:express.Request, res:express.Response){
+    res.send('Your custom info page goes here');
+});
+
+app.listen(port, function(){
+    console.log("app Server Started... on port: " + port);
+});
+
+
+
+
+
+
+
+
+
+/*
+//creating node app
 import http = require('http');
 
 var port:number = process.env.port || 3000
@@ -11,3 +39,4 @@ var server:http.Server = http.createServer(
 server.listen(port, function(){
    console.log("Server Started... Listening on port: " + port); 
 });
+*/
